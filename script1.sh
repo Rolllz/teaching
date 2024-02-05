d803f0d0f6b371f9361f8a9bbf9180ebc63a28ab
Rolllz <64433140+Rolllz@users.noreply.github.com>
2024-02-05 16:39:56 +0300

    Delete script1.sh

--- script1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lsblk > /vagrant/file1.txt
-sudo apt update
-sudo apt install -y mdadm smartmontools hdparm gdisk lshw parted
-sudo modprobe {raid{0,1,5,6,10},linear,multipath}
-sudo chmod 777 /etc/mdadm/mdadm.conf
