404ec01b3e71fe4edb9893ec36f7f01f876528e5
Rolllz <64433140+Rolllz@users.noreply.github.com>
2024-04-11 14:02:09 +0300

    Update pam.sh

--- pam.sh
+++ pam.sh
@@ -9,21 +9,16 @@ curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-useradd otusadm && useradd otus
 for i in {otus,otusadm}; do
+    useradd -s /bin/bash $i
     mkdir /home/$i
     cp -rT /etc/skel /home/$i
     chown -R $i:$i /home/$i
 done
-groupadd -f admin && groupadd -f dockerallow
+groupadd -f admin
 yes "Otus2024!" | passwd otusadm && yes "Otus2024!" | passwd otus && yes "vagrant" | passwd root
 for i in {vagrant,root,otusadm}; do usermod -aG admin $i; done
 cp -f /vagrant/login.sh /usr/local/bin/
-echo "auth required pam_exec.so debug /usr/local/bin/login.sh" > /etc/pam.d/sshd
-echo "%dockerallow ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers
-cp /etc/sudoers /etc/sudoers.bak
-cp /etc/profile /etc/profile.bak
-sed -i "s/sudo\tALL=(ALL:ALL) ALL/sudo\tALL=(ALL:ALL) NOPASSWD: ALL/g"
-echo $PATH > /tmp/path.txt
-for i in {docker,sudo,dockerallow}; do usermod -aG $i otus; done
-#systemctl daemon-reload
+echo "auth required pam_exec.so debug /usr/local/bin/login.sh" >> /etc/pam.d/sshd
+echo "otus ALL=NOPASSWD: /usr/bin/systemctl restart docker" > /etc/sudoers.d/otus
+usermod -aG docker otus
